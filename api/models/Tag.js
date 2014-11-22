cdf1cff9a5b679a80258989af7f68c2db19ca19c
Alberto Souza <alberto.souza.99@gmail.com>
2014-11-22 20:58:19 -0200

    update Term.validateAndCreateTags to Tag.validateAndCreateTags

--- api/models/Tag.js
+++ api/models/Tag.js
@@ -15,5 +15,71 @@ module.exports = {
     description: {
       type: 'text'
     }
+  },
+
+  validateAndCreateTags: function(tags, creatorId, cb) {
+    if (!tags) return cb(null, []);
+
+    var queries = [];
+    var textsSearching = {};
+
+    for (var i = 0; i < tags.length; i++) {
+      if (typeof tags[i] == 'object' ) {
+        if (tags[i].id && Number(tags[i].id)) {
+          // change it to use the id
+          tags[i] = tags[i].id;
+        } else if (tags[i].text ) {
+          if (!textsSearching[tags[i].text]) {
+            textsSearching[tags[i].text] = true;
+            queries.push(Tag.checkIfTagExistsInDbOrCreate.bind(null, tags[i].text, tags, creatorId, i));
+          } else {
+            tags[i] = null;
+          }
+        } else {
+          return cb('Invalid tag type for:' +
+            JSON.stringify(tags[i]));
+        }
+      } else if(Number(tags[i])) {
+        // is number / id
+        // is id then allow sails handle this relationship
+      } else if(typeof tags[i] == 'string') {
+        // is string / the tag text
+        if (!textsSearching[tags[i]]) {
+          textsSearching[tags[i]] = true;
+          queries.push(Tag.checkIfTagExistsInDbOrCreate.bind(null, tags[i], tags, creatorId, i));
+        } else {
+          tags[i] = null;
+        }
+      } else {
+        return cb('Invalid tag type for:' +
+          JSON.stringify(tags[i]));
+      }
+    }
+    async.parallel(queries, function(err) {
+      cb(err, _.difference(tags, [null, undefined]));
+    });
+  },
+
+  checkIfTagExistsInDbOrCreate: function checkIfTagExistsInDbOrCreate(text, tags, creatorId, i, done) {
+    Tag.findOneByText(text).exec(function(err, term) {
+      if (err) return done(err);
+
+      if (term) {
+        // term exists only swap by id
+        tags[i] = term.id;
+        return done();
+      }
+
+      Tag.create({
+        creator: creatorId,
+        text: text
+      }).exec(function(err, newTerm) {
+        if(err) return done(err);
+        if(!newTerm) return done('checkIfTagExistsInDbOrCreate: Something wrong happend in tag term creation');
+        tags[i] = newTerm.id;
+        // else the term is valid, then do nothing ...
+        done();
+      })
+    });
   }
 }
