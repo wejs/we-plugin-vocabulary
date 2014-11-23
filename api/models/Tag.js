a1650dede452c19655f4c5c071c8060755950f7d
Alberto Souza <alberto.souza.99@gmail.com>
2014-11-23 10:18:48 -0200

    update tag text check for work with async requests

--- api/models/Tag.js
+++ api/models/Tag.js
@@ -61,25 +61,47 @@ module.exports = {
   },
 
   checkIfTagExistsInDbOrCreate: function checkIfTagExistsInDbOrCreate(text, tags, creatorId, i, done) {
-    Tag.findOneByText(text).exec(function(err, term) {
-      if (err) return done(err);
-
-      if (term) {
-        // term exists only swap by id
-        tags[i] = term.id;
-        return done();
+    // TODO change this check to be only one query, one "createIfNotExists"
+    Tag.create({
+      creator: creatorId,
+      text: text
+    }).exec(function(err, newTerm) {
+      if(err) {
+        // Node.js do queries in async mode then they may create one record after run
+        //   this Tag.create query then check if the error returned is tag already exists
+        if(checkIfIsAlreadyExistsError(err)) {
+          return Tag.findOneByText(text).exec(function(err2, term) {
+             if (err2) return done(err2);
+             if (!term) return done(err);
+            tags[i] = term.id;
+            done();
+          });
+        }
+        // else is a  unknow error
+        return done(err);
       }
-
-      Tag.create({
-        creator: creatorId,
-        text: text
-      }).exec(function(err, newTerm) {
-        if(err) return done(err);
-        if(!newTerm) return done('checkIfTagExistsInDbOrCreate: Something wrong happend in tag term creation');
-        tags[i] = newTerm.id;
-        // else the term is valid, then do nothing ...
-        done();
-      })
-    });
+      if(!newTerm) return done('checkIfTagExistsInDbOrCreate: Something wrong happend in tag term creation');
+      tags[i] = newTerm.id;
+      // else the term is valid, then do nothing ...
+      done();
+    })
   }
+}
+
+/**
+ * Helper to check if err is a term already exists error
+ *
+ * @param  {object} err Waterline Error object
+ * @return {boolean}
+ */
+function checkIfIsAlreadyExistsError(err) {
+  if (  err.invalidAttributes &&
+    err.invalidAttributes.text &&
+    err.invalidAttributes.text[0] &&
+    err.invalidAttributes.text[0].rule &&
+    err.invalidAttributes.text[0].rule === 'unique'
+  ) {
+    return true;
+  }
+  return false;
 }
